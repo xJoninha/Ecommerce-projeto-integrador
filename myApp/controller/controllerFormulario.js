59e952e585d7177ac41bae46a081fd75b6d29941
Tarus622 <davideoliveira622@gmail.com>
2022-06-02 18:24:55 -0300

    feat: finalizando alterações

--- myApp/controller/controllerFormulario.js
+++ myApp/controller/controllerFormulario.js
@@ -1,21 +1,57 @@
 const { validationResult } = require('express-validator');
+const fs = require('fs');
+const { parse } = require('path');
+const path = require('path');
+const users = require('../data/users.json');
 
-const controller = {
-    index: function(req, res) {
-        res.render('cadastro')
-    },
-    register: function(req, res, next) {
+const helper = {};
+
+
+const controller = {};
+
+    controller.index = (req, res) => {res.render('cadastro')};
+
+    controller.register = (req, res, next) => {
         const errors = validationResult(req);
 
         if(!errors.isEmpty()) {
             console.log(errors.mapped())
-            return res.render('cadastro', {errors: errors.mapped()});
+        res.render('cadastro', {errors: errors.mapped()});
         } else {
-            console.log(errors)
-            return res.render('cadastro');
+
+        const user = {
+            email: req.body.email,
+            cpf: req.body.cpf,
+            nome: req.body.name,
+            sobrenome: req.body.last_name,
+            nascimento: req.body.birthday,
+            age: req.body.age,
+            celular: req.body.telephone,
+            destinatario: req.body.destinatario,
+            cep: req.body.cep,
+            logradouro: req.body.logradouro,
+            cidade_bairro: req.body.cidade_bairro,
+            numero: req.body.numero,
+            senha: req.body.password,
+            terms: req.body.terms
+
+        };
+
+        const usersObject = parse(users);
+
+        const new_user = {
+            ...user
         }
 
-    }
-}
+        const users_updated = {...users, user}
+
+        console.log(user);
+        let usersJSON = JSON.stringify(users_updated, null, 2);
+        fs.writeFileSync(path.join(__dirname, '../data/users.json'), usersJSON, 'utf-8');
+        res.render('cadastro');
+        }
+
+    } 
+
 
 module.exports = controller;
